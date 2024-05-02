8d039e2dafeebacaad2926e5d22ff8ec7fa96d76
YoungWon <nbbb9@naver.com>
2024-05-02 18:29:21 +0900

    Final 2

--- spring_outpick_pjt/src/main/java/com/encore/outpick/product/controller/ProductController.java
+++ spring_outpick_pjt/src/main/java/com/encore/outpick/product/controller/ProductController.java
@@ -2,7 +2,6 @@ package com.encore.outpick.product.controller;
 
 import com.encore.outpick.product.domain.ProductDTO;
 import com.encore.outpick.product.service.ProductService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
